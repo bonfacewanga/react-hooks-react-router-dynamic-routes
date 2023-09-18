328c7a8a026fe16812e0ec98595aaf96f56f499e
Bonface <bonniewanga34@gmail.com>
2023-09-18 04:34:58 +0300

    Completed assignments

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,10 @@ import { Route, Switch } from "react-router-dom";
 import NavBar from "./NavBar";
 import MoviesPage from "./MoviesPage";
 
+
 function App() {
   const [movies, setMovies] = useState([
+    
     { id: 1, title: "A River Runs Through It" },
     { id: 2, title: "Se7en" },
     { id: 3, title: "Inception" }
